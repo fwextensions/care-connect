75ec2e3623ac11dedc7f065c72b1fc4516e4831a
John Dunning <fw@johndunning.com>
2026-03-01 17:47:42 -0800

    Add /forms/849b/:deflectionId/html route to preview the form
    Add the /forms route to the role protected paths.
    Fix esbuild watch.

--- client/src/lesc/routes/LESCRoutes.jsx
+++ client/src/lesc/routes/LESCRoutes.jsx
@@ -14,6 +14,7 @@ import PropertyForm from '../components/PropertyForm';
 import NarcoticsForm from '../components/NarcoticsForm';
 import TestFormPreview from '@/forms/TestFormPreview';
 import Form849BPreview from '@/forms/Form849BPreview';
+import Form849BHtmlPreview from '@/forms/Form849BHtmlPreview';
 
 function LESCRoutes () {
   const { user } = useAuthContext();
@@ -34,6 +35,7 @@ function LESCRoutes () {
       <Route path='care' element={<Care />} />
       <Route path='forms/test' element={<TestFormPreview />} />
       <Route path='forms/849b/:deflectionId' element={<Form849BPreview />} />
+      <Route path='forms/849b/:deflectionId/html' element={<Form849BHtmlPreview />} />
       <Route path='' element={<Navigate to={defaultPath} />} />
     </Routes>
   );
